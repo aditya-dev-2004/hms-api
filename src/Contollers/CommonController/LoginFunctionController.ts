7c7cbb15aed760432c1518839c902f54dc1f7900
aditya-dev-2004 <dev.adityakumar92@gmail.com>
2025-04-01 07:52:10 +0530

    new

--- src/Contollers/CommonController/LoginFunctionController.ts
+++ src/Contollers/CommonController/LoginFunctionController.ts
@@ -22,24 +22,18 @@ export const userLoginController = async (req: any, res: any) => {
 export const userRegisterController = async (req: any, res: any) => {
     try {
         const dataToSave = req.body;
-        let { profile } = req.files;
-        // console.log(dataToSave);
-        
+        let { profile } = req.files; 
         const pathToSaveFile = path.join(__dirname, '../../uploads/');
         const profileName = uploadFileHelper(profile, pathToSaveFile, res);
-        const finalData: any = { ...dataToSave, profile: profileName };
-        // console.log(finalData);
+        const finalData: any = { ...dataToSave, profile: profileName }; 
+        const TblName: any = await returnUserType(dataToSave?.userType); 
+        const isExist = await TblName.findOne({ where: { email: dataToSave?.email } }); 
+        console.log(dataToSave?.experience,"35445646");
         
-        const TblName: any = await returnUserType(dataToSave?.userType);
-        // console.log(TblName);
-        
-        const isExist = await TblName.findOne({ where: { email: dataToSave?.email } });
-        // console.log(isExist);
         if (isExist) {
             return createResponse(res, 208, "User Already Exist !", isExist, false, true)
         } else {
-            const result = await TblName.save(finalData);
-            // console.log(result);
+            const result = await TblName.save(finalData); 
             if (result){
                 return createResponse(res, 201, "User register successfully !", result, true, false)
             }else{
@@ -47,8 +41,7 @@ export const userRegisterController = async (req: any, res: any) => {
             }
         }
     } catch (err) {
-        if (err) {
-            // console.log(err);
+        if (err) { 
             createResponse(res, 500, "Internal Server Error",err, false, true);
         }
     }
