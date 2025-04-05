fa4cde2fefe5664bab8c6a12fef8a84c6f69c99b
aditya-dev-2004 <dev.adityakumar92@gmail.com>
2025-04-05 19:26:56 +0530

    new

--- src/Contollers/AdminController/DepartmentController.ts
+++ src/Contollers/AdminController/DepartmentController.ts
@@ -1,9 +1,9 @@
 import { Department } from "../../Entities/DepartmentTbl"; 
 import { createResponse } from "../../Helpers/createResponse";
 export const addDepartmentController = async (req: any, res: any) => {
-    const {name}:any=req.body;
-  const isExist=await Department.findOne({where:{name}})
-  if(isExist){
+    const { name}:any=req.body; 
+  const isExist=await Department.findOne({where:{name}})             
+  if(isExist == name){
    return createResponse(res, 200, "Department Already exist !", isExist, false, true)
   }else{
    const result=await  Department.save({name});
